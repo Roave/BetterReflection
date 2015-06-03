69c43b98adfdb323669380fcbce26ef1b26717ef
James Titcumb <james@asgrim.com>
2015-06-03 19:42:10 +0100

    Use PhpParser to load the AST of the file

--- src/Investigator.php
+++ src/Investigator.php
@@ -2,6 +2,8 @@
 namespace Asgrim;
 
 use Composer\Autoload\ClassLoader;
+use PhpParser\Parser;
+use PhpParser\Lexer;
 
 class Investigator
 {
@@ -14,6 +16,17 @@ class Investigator
 
     public function investigate($className)
     {
+        $file = $this->classLoader->findFile($className);
+
+        if (class_exists($file, false)) {
+            throw new \LogicException(sprintf('Class "%s" is already loaded', $className));
+        }
+
+        $fileContent = file_get_contents($file);
+
+        $parser = new Parser(new Lexer);
+        $ast = $parser->parse($fileContent);
+
         return new ClassInfo();
     }
 }
