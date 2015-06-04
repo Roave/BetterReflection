530774fde280791637d1bf1a79318e1ee6c9fab8
James Titcumb <james@asgrim.com>
2015-06-04 14:05:26 +0100

    Search properly through the AST for classes to load

--- src/Reflector.php
+++ src/Reflector.php
@@ -5,7 +5,6 @@ use Composer\Autoload\ClassLoader;
 use PhpParser\Parser;
 use PhpParser\Lexer;
 use PhpParser\Node;
-use PhpParser\Node\Stmt;
 
 class Reflector
 {
@@ -22,6 +21,10 @@ class Reflector
      */
     public function reflect($className)
     {
+        if ('\\' == $className[0]) {
+            $className = substr($className, 1);
+        }
+
         $file = $this->classLoader->findFile($className);
 
         if (class_exists($file, false)) {
@@ -33,7 +36,52 @@ class Reflector
         $parser = new Parser(new Lexer);
         $ast = $parser->parse($fileContent);
 
-        $class = ReflectionClass::createFromNode($ast[0]->stmts[0], $ast[0]);
-        return $class;
+        $classes = $this->reflectClassesFromTree($ast);
+
+        foreach ($classes as $class) {
+            if ($class->getName() == $className) {
+                return $class;
+            }
+        }
+
+        throw new \UnexpectedValueException(sprintf('Class "%s" could not be found to load', $className));
+    }
+
+    /**
+     * @param Node\Stmt\Namespace_ $namespace
+     * @return ReflectionClass[]
+     */
+    private function reflectClassesFromNamespace(Node\Stmt\Namespace_ $namespace)
+    {
+        $classes = [];
+        foreach ($namespace->stmts as $node) {
+            if (get_class($node) == Node\Stmt\Class_::class) {
+                $classes[] = ReflectionClass::createFromNode($node, $namespace);
+            }
+        }
+        return $classes;
+    }
+
+    /**
+     * @param Node[] $ast
+     * @return ReflectionClass[]
+     */
+    private function reflectClassesFromTree(array $ast)
+    {
+        $classes = [];
+        foreach ($ast as $node) {
+            switch (get_class($node)) {
+                case Node\Stmt\Namespace_::class:
+                    $classes = array_merge(
+                        $classes,
+                        $this->reflectClassesFromNamespace($node)
+                    );
+                    break;
+                case Node\Stmt\Class_::class:
+                    $classes[] = ReflectionClass::createFromNode($node);
+                    break;
+            }
+        }
+        return $classes;
     }
 }
