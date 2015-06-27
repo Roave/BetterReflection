dbc6b39dc57aeef304e18770450ba7861b86eef8
James Titcumb <james@asgrim.com>
2015-06-27 09:33:34 +0100

    Added ReflectionParameterTest that tests various default values

---
+++ test/ReflectionParameterTest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace AsgrimTest;
+
+use Asgrim\Reflector;
+
+class ReflectionParameterTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var Reflector
+     */
+    private $reflector;
+
+    public function setUp()
+    {
+        global $loader;
+        $this->reflector = new Reflector($loader);
+    }
+
+    public function defaultParameterProvider()
+    {
+        return [
+            ['1', 1],
+            ['"hello"', 'hello'],
+            ['null', null],
+            ['1.1', 1.1],
+            ['[]', []],
+        ];
+    }
+
+    /**
+     * @dataProvider defaultParameterProvider
+     */
+    public function testDefaultParametersTypes($defaultExpression, $expectedValue)
+    {
+        $content = "<?php class Foo { public function myMethod(\$var = $defaultExpression) {} }";
+
+        $classInfo = $this->reflector->reflectClassFromString('Foo', $content);
+        $methodInfo = $classInfo->getMethod('myMethod');
+        $paramInfo = $methodInfo->getParameters()[0];
+        $actualValue = $paramInfo->getDefaultValue();
+
+        $this->assertSame($expectedValue, $actualValue);
+    }
+}
