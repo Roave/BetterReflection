7cbe7967216763b7b66909e8c676116af6fc2b1b
James Titcumb <james@asgrim.com>
2015-07-03 11:15:04 +0100

    Added coverage for ClassReflector

--- test/unit/Reflector/ClassReflectorTest.php
+++ test/unit/Reflector/ClassReflectorTest.php
@@ -4,7 +4,9 @@ namespace BetterReflectionTest\Reflector;
 
 use BetterReflection\Reflection\ReflectionClass;
 use BetterReflection\Reflector\ClassReflector;
+use BetterReflection\Reflector\Generic;
 use BetterReflection\SourceLocator\SingleFileSourceLocator;
+use BetterReflection\SourceLocator\StringSourceLocator;
 
 /**
  * @covers \BetterReflection\Reflector\ClassReflector
@@ -19,5 +21,30 @@ class ClassReflectorTest extends \PHPUnit_Framework_TestCase
 
         $this->assertContainsOnlyInstancesOf(ReflectionClass::class, $classes);
         $this->assertCount(3, $classes);
+    }
+
+    public function testReflectProxiesToGenericReflectMethod()
+    {
+        $reflector = new ClassReflector(new StringSourceLocator('<?php'));
+
+        $reflectionMock = $this->getMockBuilder(ReflectionClass::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $genericReflectorMock = $this->getMockBuilder(Generic::class)
+            ->setMethods(['reflect'])
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $genericReflectorMock->expects($this->once())
+            ->method('reflect')
+            ->will($this->returnValue($reflectionMock));
+
+        $reflectorReflection = new \ReflectionObject($reflector);
+        $reflectorReflectorReflection = $reflectorReflection->getProperty('reflector');
+        $reflectorReflectorReflection->setAccessible(true);
+        $reflectorReflectorReflection->setValue($reflector, $genericReflectorMock);
+
+        $reflector->reflect('MyClass');
     }
 }
