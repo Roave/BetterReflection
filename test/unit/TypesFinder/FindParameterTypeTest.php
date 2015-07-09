68386ff57d1e225bab0f611b989f1a2b7eaae672
James Titcumb <james@asgrim.com>
2015-07-09 09:50:59 +0100

    Moar tests

--- test/unit/TypesFinder/FindParameterTypeTest.php
+++ test/unit/TypesFinder/FindParameterTypeTest.php
@@ -2,7 +2,9 @@
 
 namespace BetterReflectionTest\TypesFinder;
 
-use BetterReflection\Reflection\ReflectionFunctionAbstract;
+use BetterReflection\Reflection\ReflectionClass;
+use BetterReflection\Reflection\ReflectionFunction;
+use BetterReflection\Reflection\ReflectionMethod;
 use BetterReflection\SourceLocator\LocatedSource;
 use BetterReflection\TypesFinder\FindParameterType;
 use PhpParser\Node\Param as ParamNode;
@@ -33,15 +35,15 @@ class FindParameterTypeTest extends \PHPUnit_Framework_TestCase
      * @param string[] $expectedInstances
      * @dataProvider parameterTypeProvider
      */
-    public function testFindParameterType($docBlock, $nodeName, $expectedInstances)
+    public function testFindParameterTypeForFunction($docBlock, $nodeName, $expectedInstances)
     {
         $node = new ParamNode($nodeName);
         $docBlock = "/**\n * $docBlock\n */";
 
-        $function = $this->getMockBuilder(ReflectionFunctionAbstract::class)
+        $function = $this->getMockBuilder(ReflectionFunction::class)
             ->setMethods(['getDocComment', 'getLocatedSource'])
             ->disableOriginalConstructor()
-            ->getMockForAbstractClass();
+            ->getMock();
 
         $function
             ->expects($this->once())
@@ -53,8 +55,54 @@ class FindParameterTypeTest extends \PHPUnit_Framework_TestCase
             ->method('getLocatedSource')
             ->will($this->returnValue(new LocatedSource('<?php', null)));
 
-        /* @var ReflectionFunctionAbstract $function */
+        /* @var ReflectionFunction $function */
         $foundTypes = (new FindParameterType())->__invoke($function, $node);
+
+        $this->assertCount(count($expectedInstances), $foundTypes);
+
+        foreach ($expectedInstances as $i => $expectedInstance) {
+            $this->assertInstanceOf($expectedInstance, $foundTypes[$i]);
+        }
+    }
+
+    /**
+     * @param string $docBlock
+     * @param string $nodeName
+     * @param string[] $expectedInstances
+     * @dataProvider parameterTypeProvider
+     */
+    public function testFindParameterTypeForMethod($docBlock, $nodeName, $expectedInstances)
+    {
+        $node = new ParamNode($nodeName);
+        $docBlock = "/**\n * $docBlock\n */";
+
+        $class = $this->getMockBuilder(ReflectionClass::class)
+            ->setMethods(['getLocatedSource'])
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $class
+            ->expects($this->once())
+            ->method('getLocatedSource')
+            ->will($this->returnValue(new LocatedSource('<?php', null)));
+
+        $method = $this->getMockBuilder(ReflectionMethod::class)
+            ->setMethods(['getDocComment', 'getDeclaringClass'])
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $method
+            ->expects($this->once())
+            ->method('getDocComment')
+            ->will($this->returnValue($docBlock));
+
+        $method
+            ->expects($this->once())
+            ->method('getDeclaringClass')
+            ->will($this->returnValue($class));
+
+        /* @var ReflectionMethod $method */
+        $foundTypes = (new FindParameterType())->__invoke($method, $node);
 
         $this->assertCount(count($expectedInstances), $foundTypes);
 
