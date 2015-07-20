df66fec639cf6b7a6533c619cfcc385afd4c161b
Marco Pivetta <ocramius@gmail.com>
2015-07-20 15:49:56 +0100

    Test asset: iterable and non-iterable classes

---
+++ test/unit/Fixture/ClassesImplementingIterators.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace
+{
+    // note: this will crash if loaded in PHP, as the Traversable interface is already in core
+    interface Traversable {}
+}
+
+namespace BetterReflectionTest\ClassesImplementingIterators
+{
+    class TraversableImplementation implements \Traversable
+    {
+    }
+
+    class NonTraversableImplementation
+    {
+    }
+}
