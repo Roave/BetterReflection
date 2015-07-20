e2b765e01b43aeb994ab2ca3a060a442eaf2818e
Marco Pivetta <ocramius@gmail.com>
2015-07-20 17:38:21 +0100

    Invalid class: class extending a trait

--- test/unit/Fixture/InvalidInheritances.php
+++ test/unit/Fixture/InvalidInheritances.php
@@ -4,7 +4,9 @@ namespace BetterReflectionTest\Fixture\InvalidInheritances
 {
     interface AInterface {}
     class AClass {}
+    trait ATrait {}
 
     interface InterfaceExtendingClass extends AClass {}
     class ClassExtendingInterface extends AInterface {}
+    class ClassExtendingTrait extends ATrait {}
 }
