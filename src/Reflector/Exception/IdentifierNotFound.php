46393354d52042d5a1046c576b843bdbf15131c6
James Titcumb <james@asgrim.com>
2015-07-20 17:27:03 +0100

    Add AggregateSourceLocator which is now used internally if the source locator specified is not already an aggregate

--- src/Reflector/Exception/IdentifierNotFound.php
+++ src/Reflector/Exception/IdentifierNotFound.php
@@ -2,6 +2,16 @@
 
 namespace BetterReflection\Reflector\Exception;
 
+use BetterReflection\Identifier\Identifier;
+
 class IdentifierNotFound extends \RuntimeException
 {
+    public static function fromIdentifier(Identifier $identifier)
+    {
+        return new self(sprintf(
+            '%s "%s" could not be found in the located source',
+            $identifier->getType()->getName(),
+            $identifier->getName()
+        ));
+    }
 }
