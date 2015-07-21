ed435480dae83030c6e9fe5d6ccb99c5474d7d3f
Marco Pivetta <ocramius@gmail.com>
2015-07-22 00:49:20 +0100

    Initial testing: internal reflector MUST support internal classes reflection

--- test/unit/SourceLocator/PhpInternalSourceLocatorTest.php
+++ test/unit/SourceLocator/PhpInternalSourceLocatorTest.php
@@ -5,20 +5,64 @@ namespace BetterReflectionTest\SourceLocator;
 use BetterReflection\Reflection\ReflectionClass;
 use BetterReflection\Reflector\ClassReflector;
 use BetterReflection\SourceLocator\PhpInternalSourceLocator;
+use ReflectionClass as PhpReflectionClass;
 
 /**
  * @covers \BetterReflection\SourceLocator\PhpInternalSourceLocator
  */
 class PhpInternalSourceLocatorTest extends \PHPUnit_Framework_TestCase
 {
-    public function testStdClass()
+    /**
+     * @dataProvider internalSymbolsProvider
+     *
+     * @param string $className
+     */
+    public function testCanReflectInternalClasses($className)
     {
-        $reflector = new ClassReflector(new PhpInternalSourceLocator());
-        $classInfo = $reflector->reflect('\stdClass');
+        /* @var $class */
+        $reflector = (new ClassReflector(new PhpInternalSourceLocator()));
 
-        $this->assertInstanceOf(ReflectionClass::class, $classInfo);
-        $this->assertSame('stdClass', $classInfo->getName());
-        $this->assertTrue($classInfo->isInternal());
-        $this->assertFalse($classInfo->isUserDefined());
+        try {
+            $class = $reflector->reflect($className);
+        } catch (\ReflectionException $e) {
+            $this->markTestIncomplete(sprintf(
+                'Can\'t reflect class "%s" due to an internal reflection exception: "%s". Consider adding a stub class',
+                $className,
+                $e->getMessage()
+            ));
+        }
+
+        $this->assertInstanceOf(ReflectionClass::class, $class);
+        $this->assertSame($className, $class->getName());
+        //$this->assertTrue($class->isInternal());
+        //$this->assertFalse($class->isUserDefined());
+    }
+
+    /**
+     * @return string[] internal symbols
+     */
+    public function internalSymbolsProvider()
+    {
+        $allSymbols = array_merge(
+            get_declared_classes(),
+            get_declared_interfaces(),
+            get_declared_traits()
+        );
+
+        $indexedSymbols = array_combine($allSymbols, $allSymbols);
+
+        return array_map(
+            function ($symbol) {
+                return [$symbol];
+            },
+            array_filter(
+                $indexedSymbols,
+                function ($symbol) {
+                    $reflection = new PhpReflectionClass($symbol);
+
+                    return $reflection->isInternal();
+                }
+            )
+        );
     }
 }
