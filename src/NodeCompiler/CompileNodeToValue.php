ab1bac362926d0c7468204f104c4aacb24955633
James Titcumb <james@asgrim.com>
2015-08-03 09:10:56 +0100

    Factor out into private methods

--- src/NodeCompiler/CompileNodeToValue.php
+++ src/NodeCompiler/CompileNodeToValue.php
@@ -27,44 +27,75 @@ class CompileNodeToValue
         }
 
         if ($node instanceof Node\Expr\Array_) {
-            $compiledArray = [];
-            foreach ($node->items as $arrayItem) {
-                $compiledValue = $this->__invoke($arrayItem->value);
-
-                if (null == $arrayItem->key) {
-                    $compiledArray[] = $compiledValue;
-                    continue;
-                }
-
-                $compiledArray[$this->__invoke($arrayItem->key)] = $compiledValue;
-            }
-            return $compiledArray;
+            return $this->compileArray($node);
         }
 
         if ($node instanceof Node\Expr\ConstFetch) {
-            $firstName = reset($node->name->parts);
-            switch ($firstName) {
-                case 'null':
-                    return null;
-                case 'false':
-                    return false;
-                case 'true':
-                    return true;
-                default:
-                    // @todo this should evaluate the VALUE, not the name
-                    /* @see https://github.com/Roave/BetterReflection/issues/19 */
-                    return $firstName;
-            }
+            return $this->compileConstFetch($node);
         }
 
         if ($node instanceof Node\Expr\ClassConstFetch) {
-            // @todo this should evaluate the VALUE, not the name
-            /* @see https://github.com/Roave/BetterReflection/issues/19 */
-            $className = implode('\\', $node->class->parts);
-            $constName = $node->name;
-            return $className . '::' . $constName;
+            return $this->compileClassConstFetch($node);
         }
 
         throw new Exception\UnableToCompileNode('Unable to compile expression: ' . get_class($node));
+    }
+
+    /**
+     * Compile arrays
+     *
+     * @param Node\Expr\Array_ $arrayNode
+     * @return array
+     */
+    private function compileArray(Node\Expr\Array_ $arrayNode)
+    {
+        $compiledArray = [];
+        foreach ($arrayNode->items as $arrayItem) {
+            $compiledValue = $this->__invoke($arrayItem->value);
+
+            if (null == $arrayItem->key) {
+                $compiledArray[] = $compiledValue;
+                continue;
+            }
+
+            $compiledArray[$this->__invoke($arrayItem->key)] = $compiledValue;
+        }
+        return $compiledArray;
+    }
+
+    /**
+     * Compile constant expressions
+     *
+     * @param Node\Expr\ConstFetch $constNode
+     * @return bool|null
+     */
+    private function compileConstFetch(Node\Expr\ConstFetch $constNode)
+    {
+        $firstName = reset($constNode->name->parts);
+        switch ($firstName) {
+            case 'null':
+                return null;
+            case 'false':
+                return false;
+            case 'true':
+                return true;
+            default:
+                throw new Exception\UnableToCompileNode('Unable to compile constant expressions');
+        }
+    }
+
+    /**
+     * Compile class constants
+     *
+     * @param Node\Expr\ClassConstFetch $node
+     * @return string
+     */
+    private function compileClassConstFetch(Node\Expr\ClassConstFetch $node)
+    {
+        // @todo this should evaluate the VALUE, not the name
+        /* @see https://github.com/Roave/BetterReflection/issues/19 */
+        $className = implode('\\', $node->class->parts);
+        $constName = $node->name;
+        return $className . '::' . $constName;
     }
 }
