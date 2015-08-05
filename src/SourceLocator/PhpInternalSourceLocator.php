18d9cdc00c712a57dd3dd153b30d81f45ba16484
James Titcumb <james@asgrim.com>
2015-08-05 09:44:07 +0100

    Also allow underscores in class names

--- src/SourceLocator/PhpInternalSourceLocator.php
+++ src/SourceLocator/PhpInternalSourceLocator.php
@@ -73,7 +73,7 @@ class PhpInternalSourceLocator implements SourceLocator
      */
     private function buildStubName($className)
     {
-        if (!preg_match('/^[a-zA-Z]+$/', $className)) {
+        if (!preg_match('/^[a-zA-Z_]+$/', $className)) {
             return null;
         }
 
