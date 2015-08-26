d5e4991ff294be1590648f0168ef38b7b3fefa8f
Marco Pivetta <ocramius@gmail.com>
2015-08-26 05:36:07 +0100

    Testing that all stubbed classes are in sync with their internal counterparts

--- test/unit/SourceLocator/Type/PhpInternalSourceLocatorTest.php
+++ test/unit/SourceLocator/Type/PhpInternalSourceLocatorTest.php
@@ -5,6 +5,7 @@ namespace BetterReflectionTest\SourceLocator\Type;
 use BetterReflection\Identifier\Identifier;
 use BetterReflection\Identifier\IdentifierType;
 use BetterReflection\Reflection\ReflectionClass;
+use BetterReflection\Reflection\ReflectionMethod;
 use BetterReflection\Reflector\ClassReflector;
 use BetterReflection\Reflector\Reflector;
 use BetterReflection\SourceLocator\Located\InternalLocatedSource;
@@ -143,5 +144,73 @@ class PhpInternalSourceLocatorTest extends \PHPUnit_Framework_TestCase
                 )
             )
         );
+    }
+
+    /**
+     * @dataProvider stubbedClassesProvider
+     *
+     * @param string $className
+     */
+    public function testAllGeneratedStubsAreInSyncWithInternalReflectionClasses($className)
+    {
+        if (! (
+            class_exists($className, false)
+            || interface_exists($className, false)
+            || trait_exists($className, false)
+        )) {
+            $this->markTestSkipped(sprintf('Class "%s" is not available in this environment', $className));
+        }
+
+        $reflector = new ClassReflector(new PhpInternalSourceLocator());
+
+        $this->assertSameClassAttributes(new \ReflectionClass($className), $reflector->reflect($className));
+    }
+
+    /**
+     * @return string[][]
+     */
+    public function stubbedClassesProvider()
+    {
+        $classNames = array_filter(
+            str_replace('.stub', '', scandir(__DIR__ . '/../../../stub')),
+            function ($fileName) {
+                return trim($fileName, '.');
+            }
+        );
+
+        return array_combine(
+            $classNames,
+            array_map(
+                function ($fileName) {
+                    return [$fileName];
+                },
+                $classNames
+            )
+        );
+    }
+
+    private function assertSameClassAttributes(\ReflectionClass $original, ReflectionClass $stubbed)
+    {
+        $this->assertSame($original->getName(), $stubbed->getName());
+
+        $internalParent     = $original->getParentClass();
+        $betterParent       = $stubbed->getParentClass();
+        $internalParentName = $internalParent ? $internalParent->getName() : null;
+        $betterParentName   = $betterParent ? $betterParent->getName() : null;
+
+        $this->assertSame($internalParentName, $betterParentName);
+        //$this->assertCount(count($original->getMethods()), $stubbed->getMethods());
+
+        $this->assertEquals($original->getConstants(), $stubbed->getConstants());
+
+        foreach ($original->getMethods() as $method) {
+            $this->assertSameMethodAttributes($method, $stubbed->getMethod($method->getName()));
+        }
+    }
+
+    private function assertSameMethodAttributes(\ReflectionMethod $original, ReflectionMethod $stubbed)
+    {
+        $this->markTestIncomplete('TBD - #107 required');
+        // @TODO once https://github.com/Roave/BetterReflection/issues/107 is handled
     }
 }
