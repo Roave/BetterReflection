dbdc087c3db9c1c4e074d6b65e238f57e16cbc12
Marco Pivetta <ocramius@gmail.com>
2015-08-31 04:07:34 +0100

    #119 - adding code to turn generated class stubs into interfaces or traits depending on reflection

--- src/SourceLocator/PhpInternalSourceLocator.php
+++ src/SourceLocator/PhpInternalSourceLocator.php
@@ -3,6 +3,11 @@
 namespace BetterReflection\SourceLocator;
 
 use BetterReflection\Identifier\Identifier;
+use PhpParser\Node\Stmt\Class_;
+use PhpParser\Node\Stmt\Interface_;
+use PhpParser\Node\Stmt\Trait_;
+use PhpParser\ParserFactory;
+use PhpParser\PrettyPrinter\Standard;
 use Zend\Code\Generator\ClassGenerator;
 use Zend\Code\Reflection\ClassReflection;
 
@@ -21,9 +26,35 @@ class PhpInternalSourceLocator implements SourceLocator
             return new InternalLocatedSource("<?php\n\n" . $stub);
         }
 
-        return new InternalLocatedSource(
-            "<?php\n\n" . ClassGenerator::fromReflection(new ClassReflection($name))->generate()
-        );
+        $reflection = new ClassReflection($name);
+        $stubCode   = ClassGenerator::fromReflection($reflection)->generate();
+        $interface  = $reflection->isInterface();
+        $trait      = $reflection->isTrait();
+
+        if ($interface || $trait) {
+            $statements = (new ParserFactory())
+                ->create(ParserFactory::PREFER_PHP7)
+                ->parse('<?php ' . $stubCode);
+
+            foreach ($statements as $key => $statement) {
+                if ($statement instanceof Class_) {
+                    $statements[$key] = $interface
+                        ? new Interface_(
+                            $statement->name,
+                            [
+                                'extends' => $statement->implements,
+                                'stmts'   => $statement->stmts,
+                            ]
+                        )
+                        : new Trait_($statement->name, $statement->stmts);
+                }
+            }
+
+            $stubCode = (new Standard())
+                ->prettyPrint($statements);
+        }
+
+        return new InternalLocatedSource("<?php\n\n" . $stubCode);
     }
 
     /**
