8b07696bce5e86f1321c71146c1d26cd6a3ecea9
Marco Pivetta <ocramius@gmail.com>
2015-08-31 04:01:22 +0100

    #119 - stronger assertions around whether a internal-reflected class should be an interface or a trait

--- test/unit/SourceLocator/PhpInternalSourceLocatorTest.php
+++ test/unit/SourceLocator/PhpInternalSourceLocatorTest.php
@@ -70,6 +70,11 @@ class PhpInternalSourceLocatorTest extends \PHPUnit_Framework_TestCase
         $this->assertSame($className, $class->getName());
         $this->assertTrue($class->isInternal());
         $this->assertFalse($class->isUserDefined());
+
+        $internalReflection = new \ReflectionClass($class);
+
+        $this->assertSame($internalReflection->isInterface(), $class->isInterface());
+        $this->assertSame($internalReflection, $class->isTrait());
     }
 
     /**
