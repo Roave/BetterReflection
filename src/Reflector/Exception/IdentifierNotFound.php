d4710d461ee97c5d8ded553e4493a9e706d8df01
Robert Schönthal <caziel@gmx.net>
2015-09-17 14:23:12 +0200

    added identifier accessor for IdentifierNotFoundException

--- src/Reflector/Exception/IdentifierNotFound.php
+++ src/Reflector/Exception/IdentifierNotFound.php
@@ -6,12 +6,32 @@ use BetterReflection\Identifier\Identifier;
 
 class IdentifierNotFound extends \RuntimeException
 {
+    /**
+     * @var Identifier
+     */
+    private $identifier;
+
+    public function __construct($message, Identifier $identifier)
+    {
+        parent::__construct($message);
+
+        $this->identifier = $identifier;
+    }
+
+    /**
+     * @return Identifier
+     */
+    public function getIdentifier()
+    {
+        return $this->identifier;
+    }
+
     public static function fromIdentifier(Identifier $identifier)
     {
         return new self(sprintf(
             '%s "%s" could not be found in the located source',
             $identifier->getType()->getName(),
             $identifier->getName()
-        ));
+        ), $identifier);
     }
 }
