1bffe5db8626dda054c34bd183e3a66210a3d6b2
James Titcumb <james@asgrim.com>
2015-11-06 09:54:55 +0000

    Split things out and provide README.md

---
+++ demo/README.md
@@ -0,0 +1,11 @@
+Demo Files
+==========
+
+Each folder contains demo files which are designed to be run on the command line.
+
+For example:
+
+```shell
+$ cd demo/basic-reflection
+$ php example1.php
+```
---
+++ demo/basic-reflection/example3.php
@@ -0,0 +1,20 @@
+<?php
+
+// Loading a specific file (not from autoloader)
+
+require_once __DIR__ . '/../../vendor/autoload.php';
+
+use BetterReflection\Reflector\ClassReflector;
+use BetterReflection\SourceLocator\Type\AggregateSourceLocator;
+use BetterReflection\SourceLocator\Type\SingleFileSourceLocator;
+
+$reflector = new ClassReflector(new AggregateSourceLocator([
+    new SingleFileSourceLocator('assets/MyClass.php'),
+]));
+
+$reflection = $reflector->reflect('MyClass');
+
+var_dump($reflection->getName()); // MyClass
+var_dump($reflection->getProperty('foo')->isPrivate()); // true
+var_dump($reflection->getProperty('foo')->getDocBlockTypeStrings()[0]); // string
+var_dump($reflection->getMethod('getFoo')->getDocBlockReturnTypes()[0]->__toString()); // string
