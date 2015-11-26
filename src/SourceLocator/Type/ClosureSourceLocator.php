c066f3f2dac7d7b0bdaadd7d01307d0af3242c19
James Titcumb <james@asgrim.com>
2015-11-26 16:39:34 +0000

    Added ClosureSourceLocator and test

---
+++ src/SourceLocator/Type/ClosureSourceLocator.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace BetterReflection\SourceLocator\Type;
+
+use Closure;
+use BetterReflection\Identifier\Identifier;
+use BetterReflection\Identifier\IdentifierType;
+use BetterReflection\Reflector\Reflector;
+use BetterReflection\SourceLocator\Ast\Strategy\AstConversionStrategy;
+use BetterReflection\SourceLocator\Ast\Strategy\NodeToReflection;
+use BetterReflection\SourceLocator\Located\LocatedSource;
+use SuperClosure\Analyzer\AstAnalyzer;
+use SuperClosure\Analyzer\ClosureAnalyzer;
+use PhpParser\Node\Expr\Closure as ClosureNode;
+
+class ClosureSourceLocator implements SourceLocator
+{
+    /**
+     * @var Closure
+     */
+    private $closure;
+
+    /**
+     * @var ClosureAnalyzer
+     */
+    private $closureAnalyzer;
+
+    /**
+     * @var AstConversionStrategy
+     */
+    private $conversionStrategy;
+
+    public function __construct(Closure $closure, ClosureAnalyzer $closureAnalyzer = null, AstConversionStrategy $conversionStrategy = null)
+    {
+        $this->closure = $closure;
+
+        if (null === $closureAnalyzer) {
+            $closureAnalyzer = new AstAnalyzer();
+        }
+        $this->closureAnalyzer = $closureAnalyzer;
+
+        if (null === $conversionStrategy) {
+            $conversionStrategy = new NodeToReflection();
+        }
+        $this->conversionStrategy = $conversionStrategy;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function locateIdentifier(Reflector $reflector, Identifier $identifier)
+    {
+        $closureData = $this->closureAnalyzer->analyze($this->closure);
+
+        if (!isset($closureData['ast']) || !($closureData['ast'] instanceof ClosureNode)) {
+            return null;
+        }
+
+        $locatedSource = new LocatedSource(
+            '<?php ' . $closureData['code'],
+            $closureData['reflection']->getFileName()
+        );
+
+        return $this->conversionStrategy->__invoke(
+            $reflector,
+            $closureData['ast'],
+            $locatedSource,
+            null
+        );
+    }
+
+    public function locateIdentifiersByType(Reflector $reflector, IdentifierType $identifierType)
+    {
+        // @TODO: Implement locateIdentifiersByType() method.
+    }
+}
---
+++ test/unit/SourceLocator/Type/ClosureSourceLocatorTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace BetterReflectionTest\SourceLocator\Type;
+
+use BetterReflection\Identifier\Identifier;
+use BetterReflection\Identifier\IdentifierType;
+use BetterReflection\Reflection\ReflectionFunction;
+use BetterReflection\Reflector\Reflector;
+use BetterReflection\SourceLocator\Type\ClosureSourceLocator;
+
+/**
+ * @covers \BetterReflection\SourceLocator\Type\ClosureSourceLocator
+ */
+class ClosureSourceLocatorTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @return Reflector|\PHPUnit_Framework_MockObject_MockObject
+     */
+    private function getMockReflector()
+    {
+        return $this->getMock(Reflector::class);
+    }
+
+    public function testClosureSourceLocator()
+    {
+        $closure = function () {
+            echo "Hello world!";
+        };
+
+        $locator = new ClosureSourceLocator($closure);
+
+        /** @var ReflectionFunction $reflection */
+        $reflection = $locator->locateIdentifier(
+            $this->getMockReflector(),
+            new Identifier(
+                'Foo',
+                new IdentifierType(IdentifierType::IDENTIFIER_FUNCTION)
+            )
+        );
+
+        $this->assertSame('{closure}', $reflection->getName());
+        $this->assertContains('Hello world!', $reflection->getLocatedSource()->getSource());
+    }
+}
