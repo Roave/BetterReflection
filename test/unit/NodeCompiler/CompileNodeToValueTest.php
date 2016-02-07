41fd0b5cac1e8fb884fed258d18eba5028a7c957
James Titcumb <james@asgrim.com>
2016-02-07 12:40:13 +0000

    Added two more test cases for alias and out-of-scope ::class resolution

--- test/unit/NodeCompiler/CompileNodeToValueTest.php
+++ test/unit/NodeCompiler/CompileNodeToValueTest.php
@@ -284,4 +284,38 @@ class CompileNodeToValueTest extends \PHPUnit_Framework_TestCase
         $classInfo = $reflector->reflect('Bar\Bat');
         $this->assertSame('Bar\Foo', $classInfo->getConstant('QUX'));
     }
+
+    public function testClassConstantClassNameOutOfScopeResolution()
+    {
+        $phpCode = '<?php
+        namespace Bar;
+
+        use My\Awesome\Foo;
+
+        class Bat {
+            const QUX = Foo::class;
+        }
+        ';
+
+        $reflector = new ClassReflector(new StringSourceLocator($phpCode));
+        $classInfo = $reflector->reflect('Bar\Bat');
+        $this->assertSame('My\Awesome\Foo', $classInfo->getConstant('QUX'));
+    }
+
+    public function testClassConstantClassNameAliasedResolution()
+    {
+        $phpCode = '<?php
+        namespace Bar;
+
+        use My\Awesome\Foo as FooAlias;
+
+        class Bat {
+            const QUX = FooAlias::class;
+        }
+        ';
+
+        $reflector = new ClassReflector(new StringSourceLocator($phpCode));
+        $classInfo = $reflector->reflect('Bar\Bat');
+        $this->assertSame('My\Awesome\Foo', $classInfo->getConstant('QUX'));
+    }
 }
