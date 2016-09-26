0b3429ef15cf558a990baebb8a74a03360b0e5f3
Marco Pivetta <ocramius@gmail.com>
2016-09-26 17:33:28 +0200

    #214 removed private API tests (very risky!)

--- test/unit/SourceLocator/Type/FileIteratorSourceLocatorTest.php
+++ test/unit/SourceLocator/Type/FileIteratorSourceLocatorTest.php
@@ -55,41 +55,4 @@ class FileIteratorSourceLocatorTest extends \PHPUnit_Framework_TestCase
         self::assertEquals(DirectoryScannerAssets\Bar\FooBar::class, $classNames[0]);
         self::assertEquals(DirectoryScannerAssets\Foo::class, $classNames[1]);
     }
-
-    public function testScanDirectoryFiles()
-    {
-        $fileSystemIteratorSourceLocator = $this->sourceLocator;
-        $class = new \ReflectionClass(get_class($fileSystemIteratorSourceLocator));
-        $method = $class->getMethod('scan');
-        $method->setAccessible(true);
-        $result = $method->invoke($fileSystemIteratorSourceLocator);
-        self::assertCount(3, $result);
-
-        // test file path
-        $files = [];
-        foreach ($result as $file) {
-            $class = new \ReflectionClass('BetterReflection\SourceLocator\Type\SingleFileSourceLocator');
-            $property = $class->getProperty('filename');
-            $property->setAccessible(true);
-            $files[] = realpath($property->getValue($file));
-        }
-        sort($files);
-        self::assertEquals(realpath(__DIR__ . '/../../Assets/DirectoryScannerAssets/Bar/Empty.php'), $files[0]);
-        self::assertEquals(realpath(__DIR__ . '/../../Assets/DirectoryScannerAssets/Bar/FooBar.php'), $files[1]);
-        self::assertEquals(realpath(__DIR__ . '/../../Assets/DirectoryScannerAssets/Foo.php'), $files[2]);
-
-        // test class names
-        $classNames = [];
-        foreach ($result as $file) {
-            /* @var $file SingleFileSourceLocator */
-            $reflector = new ClassReflector($file);
-            foreach ($reflector->getAllClasses() as $class) {
-                $classNames[] = $class->getName();
-            }
-        }
-        sort($classNames);
-        self::assertEquals('BetterReflectionTest\Assets\DirectoryScannerAssets\Bar\FooBar', $classNames[0]);
-        self::assertEquals('BetterReflectionTest\Assets\DirectoryScannerAssets\Foo', $classNames[1]);
-        self::assertCount(2, $classNames);
-    }
 }
