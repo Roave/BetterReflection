fc9a9120f86da19787c702ac7a7f5dfcac262b90
Marco Pivetta <ocramius@gmail.com>
2016-09-26 16:19:37 +0200

    #214 CS (inlining expressions, docblock spacing)

--- src/SourceLocator/Exception/InvalidFileInfo.php
+++ src/SourceLocator/Exception/InvalidFileInfo.php
@@ -6,11 +6,14 @@ class InvalidFileInfo extends \RuntimeException
 {
     /**
      * @param mixed $nonSplFileInfo
+     *
      * @return InvalidFileInfo
      */
     public static function fromNonSplFileInfo($nonSplFileInfo)
     {
-        $type = is_object($nonSplFileInfo) ? get_class($nonSplFileInfo) : gettype($nonSplFileInfo) ;
-        return new self(sprintf('Expected an iterator of SplFileInfo instances, %s given instead', $type));
+        return new self(sprintf(
+            'Expected an iterator of SplFileInfo instances, %s given instead',
+            is_object($nonSplFileInfo) ? get_class($nonSplFileInfo) : gettype($nonSplFileInfo)
+        ));
     }
 }
