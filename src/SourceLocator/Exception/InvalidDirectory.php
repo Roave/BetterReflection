ced49c260663275c06d039a899be841193345bbb
Marco Pivetta <ocramius@gmail.com>
2016-09-26 16:18:17 +0200

    #214 CS (inlining expressions, docblock spacing, adding spacing)

--- src/SourceLocator/Exception/InvalidDirectory.php
+++ src/SourceLocator/Exception/InvalidDirectory.php
@@ -6,23 +6,28 @@ class InvalidDirectory extends \RuntimeException
 {
     /**
      * @param string $nonDirectory
+     *
      * @return InvalidDirectory
      */
     public static function fromNonDirectory($nonDirectory)
     {
-        if (!file_exists($nonDirectory)) {
+        if (! file_exists($nonDirectory)) {
             return new self(sprintf('%s does not exists', $nonDirectory));
         }
+
         return new self(sprintf('%s is must to be a directory not a file', $nonDirectory));
     }
 
     /**
      * @param mixed $nonStringValue
+     *
      * @return InvalidDirectory
      */
     public static function fromNonStringValue($nonStringValue)
     {
-        $type = is_object($nonStringValue) ? get_class($nonStringValue) : gettype($nonStringValue) ;
-        return new self(sprintf('Expected string, %s given', $type));
+        return new self(sprintf(
+            'Expected string, %s given',
+            is_object($nonStringValue) ? get_class($nonStringValue) : gettype($nonStringValue)
+        ));
     }
 }
