da46da632a94ca20b4c67a88ed9bf6f105ba5802
Marco Pivetta <ocramius@gmail.com>
2016-09-26 17:16:36 +0200

    #214 removing duplicate `InvalidDirectory` tests

--- test/unit/SourceLocator/Exception/InvalidDirectoryTest.php
+++ test/unit/SourceLocator/Exception/InvalidDirectoryTest.php
@@ -56,37 +56,4 @@ class InvalidDirectoryTest extends \PHPUnit_Framework_TestCase
         self::assertInstanceOf(InvalidDirectory::class, $exception);
         self::assertSame(sprintf('"%s" must be a directory, not a file', __FILE__), $exception->getMessage());
     }
-
-    public function testExceptionMessage()
-    {
-        $e = InvalidDirectory::fromNonDirectory('testDir');
-        $this->assertEquals(sprintf('%s does not exists', 'testDir'), $e->getMessage());
-
-        $e = InvalidDirectory::fromNonDirectory(__FILE__);
-        $this->assertEquals(sprintf('%s is must to be a directory not a file', __FILE__), $e->getMessage());
-
-        $e = InvalidDirectory::fromNonStringValue(new \stdClass());
-        $expected = 'Expected string, stdClass given';
-        $this->assertEquals($expected, $e->getMessage());
-
-        $e = InvalidDirectory::fromNonStringValue(true);
-        $expected = 'Expected string, boolean given';
-        $this->assertEquals($expected, $e->getMessage());
-
-        $e = InvalidDirectory::fromNonStringValue(null);
-        $expected = 'Expected string, NULL given';
-        $this->assertEquals($expected, $e->getMessage());
-
-        $e = InvalidDirectory::fromNonStringValue(100);
-        $expected = 'Expected string, integer given';
-        $this->assertEquals($expected, $e->getMessage());
-
-        $e = InvalidDirectory::fromNonStringValue(100.35);
-        $expected = 'Expected string, double given';
-        $this->assertEquals($expected, $e->getMessage());
-
-        $e = InvalidDirectory::fromNonStringValue([100, 200]);
-        $expected = 'Expected string, array given';
-        $this->assertEquals($expected, $e->getMessage());
-    }
 }
