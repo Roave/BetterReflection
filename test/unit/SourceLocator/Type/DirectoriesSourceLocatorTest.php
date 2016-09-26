2d5867be7b3fc113d491c6efedd4d15b49892e6e
Marco Pivetta <ocramius@gmail.com>
2016-09-26 17:38:10 +0200

    #214 moved invalid scenarios into the `DirectoriesSourceLocatorTest`

--- test/unit/SourceLocator/Type/DirectoriesSourceLocatorTest.php
+++ test/unit/SourceLocator/Type/DirectoriesSourceLocatorTest.php
@@ -5,6 +5,7 @@ namespace BetterReflectionTest\SourceLocator\Type;
 use BetterReflection\Identifier\IdentifierType;
 use BetterReflection\Reflection\ReflectionClass;
 use BetterReflection\Reflector\ClassReflector;
+use BetterReflection\SourceLocator\Exception\InvalidDirectory;
 use BetterReflection\SourceLocator\Type\DirectoriesSourceLocator;
 use BetterReflectionTest\Assets\DirectoryScannerAssets;
 use BetterReflectionTest\Assets\DirectoryScannerAssetsFoo;
@@ -49,5 +50,39 @@ class DirectoriesSourceLocatorTest extends \PHPUnit_Framework_TestCase
         self::assertEquals(DirectoryScannerAssetsFoo\Foo::class, $classNames[1]);
         self::assertEquals(DirectoryScannerAssets\Bar\FooBar::class, $classNames[2]);
         self::assertEquals(DirectoryScannerAssets\Foo::class, $classNames[3]);
+    }
+
+    /**
+     * @dataProvider invalidDirectoriesProvider
+     *
+     * @param array $directories
+     */
+    public function testInvalidDirectory(array $directories)
+    {
+        $this->expectException(InvalidDirectory::class);
+
+        new DirectoriesSourceLocator($directories);
+    }
+
+    public function invalidDirectoriesProvider()
+    {
+        $validDir = __DIR__ . '/../../Assets/DirectoryScannerAssets';
+
+        return [
+            [[__DIR__ . '/' . uniqid('nonExisting', true)]],
+            [[__FILE__]],
+            [[1]],
+            [[1.23]],
+            [[true]],
+            [[new \stdClass()]],
+            [[null]],
+            [[$validDir, __DIR__ . '/' . uniqid('nonExisting', true)]],
+            [[$validDir, __FILE__]],
+            [[$validDir, 1]],
+            [[$validDir, 1.23]],
+            [[$validDir, true]],
+            [[$validDir, new \stdClass()]],
+            [[$validDir, null]],
+        ];
     }
 }
