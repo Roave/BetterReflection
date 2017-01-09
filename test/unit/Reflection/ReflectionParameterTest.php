71e09872122ee3e0d310122a63c35f23a9f5d3f4
Jefersson Nathan <admin@phpse.net>
2017-01-09 15:31:54 -0300

    #240 — Added test case to reproduce the bug

--- test/unit/Reflection/ReflectionParameterTest.php
+++ test/unit/Reflection/ReflectionParameterTest.php
@@ -549,4 +549,20 @@ class ReflectionParameterTest extends \PHPUnit_Framework_TestCase
         $this->expectException(Uncloneable::class);
         $unused = clone $paramInfo;
     }
+
+    public function testGetClassFromSelfTypeHintedProperty()
+    {
+        $content = '<?php class Foo { public function myMethod(self $param) {} }';
+
+        $reflector = new ClassReflector(new AggregateSourceLocator([
+            new PhpInternalSourceLocator(),
+            new StringSourceLocator($content),
+        ]));
+        $classInfo = $reflector->reflect('Foo');
+        $methodInfo = $classInfo->getMethod('myMethod');
+
+        $hintedClassReflection = $methodInfo->getParameter('param')->getClass();
+        $this->assertInstanceOf(ReflectionClass::class, $hintedClassReflection);
+        $this->assertSame('Foo', $hintedClassReflection->getName());
+    }
 }
