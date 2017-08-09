cad3a2ae499a15631c3494e32a37c42ee93b3091
Vlasta Vesely <vlastavesely@protonmail.ch>
2017-08-09 10:55:06 +0200

    TypeFinder: add support for indexed @param annotations

--- src/TypesFinder/FindParameterType.php
+++ src/TypesFinder/FindParameterType.php
@@ -8,6 +8,7 @@ use phpDocumentor\Reflection\Types\Context;
 use phpDocumentor\Reflection\Types\ContextFactory;
 use PhpParser\Node\Param as ParamNode;
 use phpDocumentor\Reflection\DocBlock;
+use phpDocumentor\Reflection\DocBlock\Tags\Param;
 use phpDocumentor\Reflection\Type;
 use Roave\BetterReflection\Reflection\ReflectionFunctionAbstract;
 
@@ -43,10 +44,28 @@ class FindParameterType
         foreach ($paramTags as $paramTag) {
             /* @var $paramTag \phpDocumentor\Reflection\DocBlock\Tags\Param */
             if ($paramTag->getVariableName() === $node->name) {
-                return (new ResolveTypes())->__invoke(explode('|', $paramTag->getType()), $context);
+                return $this->resolveFromTag($paramTag, $context);
             }
         }
+
+        foreach ($function->getParameters() as $index => $parameter) {
+            /* @var $parameter \Roave\BetterReflection\Reflection\ReflectionParameter */
+            if ($parameter->getName() === $node->name) {
+                if (isset($paramTags[$index]) && !$paramTags[$index]->getVariableName()) {
+                    return $this->resolveFromTag($paramTags[$index], $context);
+                }
+            }
+        }
+
         return [];
+    }
+
+    /**
+     * @return Type[]
+     */
+    private function resolveFromTag(Param $paramTag, Context $context): array
+    {
+        return (new ResolveTypes())->__invoke(explode('|', $paramTag->getType()), $context);
     }
 
     /**
