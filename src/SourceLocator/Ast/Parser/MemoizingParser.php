d9bb32008d1d33ed8cf21dc23f8e8e799537cee7
Marco Pivetta <ocramius@gmail.com>
2017-08-19 14:34:24 +0200

    Implemented a memoizing AST parser

---
+++ src/SourceLocator/Ast/Parser/MemoizingParser.php
@@ -0,0 +1,44 @@
+<?php
+declare(strict_types=1);
+
+namespace Roave\BetterReflection\SourceLocator\Ast\Parser;
+
+use PhpParser\ErrorHandler;
+use PhpParser\Node;
+use PhpParser\Parser;
+
+/**
+ * @internal
+ */
+final class MemoizingParser implements Parser
+{
+    /**
+     * @var Node[][]|null[] indexed by source hash
+     */
+    private $sourceHashToAst = [];
+
+    /**
+     * @var Parser
+     */
+    private $wrappedParser;
+
+    public function __construct(Parser $wrappedParser)
+    {
+        $this->wrappedParser = $wrappedParser;
+    }
+
+    public function parse($code, ErrorHandler $errorHandler = null)
+    {
+        // note: this code is mathematically buggy by default, as we are using a hash to identify
+        //       cache entries. The string length is added to further reduce likeliness (although
+        //       already imperceptible) of key collisions.
+        //       In the "real world", this code will work just fine.
+        $hash = \sha1($code) . ':' . \strlen($code);
+
+        if (\array_key_exists($hash, $this->sourceHashToAst)) {
+            return $this->sourceHashToAst[$hash];
+        }
+
+        return $this->sourceHashToAst[$hash] = $this->wrappedParser->parse($code, $errorHandler);
+    }
+}
