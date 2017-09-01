5b002662740b5b65d4d0456eda4883dd7528bb3f
Marco Pivetta <ocramius@gmail.com>
2017-09-01 07:03:10 +0200

    Correcting examples around basic reflection - reverted class name being reflected

--- demo/basic-reflection/example2.php
+++ demo/basic-reflection/example2.php
@@ -3,10 +3,11 @@
 // Load an autoloadable class
 
 use Roave\BetterReflection\BetterReflection;
+use Roave\BetterReflection\Reflection\ReflectionClass;
 
 require_once __DIR__ . '/../../vendor/autoload.php';
 
-$reflection = (new BetterReflection())->classReflector()->reflect(\stdClass::class);
+$reflection = (new BetterReflection())->classReflector()->reflect(ReflectionClass::class);
 
 echo $reflection->getName() . "\n"; // ReflectionClass
 echo ($reflection->isInternal() === true ? 'internal' : 'not internal') . "\n"; // not internal
