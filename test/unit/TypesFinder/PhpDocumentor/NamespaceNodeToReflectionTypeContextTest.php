2785b7eefb1e53928e724ec845e7de86f7b08375
Marco Pivetta <ocramius@gmail.com>
2017-09-02 23:18:13 +0200

    Basic testing around `NamespaceNodeToReflectionTypeContext`

---
+++ test/unit/TypesFinder/PhpDocumentor/NamespaceNodeToReflectionTypeContextTest.php
@@ -0,0 +1,33 @@
+<?php
+declare(strict_types=1);
+
+namespace Roave\BetterReflectionTest\TypesFinder\PhpDocumentor;
+
+use phpDocumentor\Reflection\Types\Context;
+use PhpParser\Node\Stmt\Namespace_;
+use PHPUnit\Framework\TestCase;
+use Roave\BetterReflection\TypesFinder\PhpDocumentor\NamespaceNodeToReflectionTypeContext;
+
+/**
+ * @covers \Roave\BetterReflection\TypesFinder\PhpDocumentor\NamespaceNodeToReflectionTypeContext
+ */
+class NamespaceNodeToReflectionTypeContextTest extends TestCase
+{
+    /**
+     * @dataProvider expectedContextsProvider
+     */
+    public function testConversion(?Namespace_ $namespace, Context $expectedContext) : void
+    {
+        self::assertEquals($expectedContext, (new NamespaceNodeToReflectionTypeContext())->__invoke($namespace));
+    }
+
+    public function expectedContextsProvider() : array
+    {
+        return [
+            'No namespace' => [
+                null,
+                new Context(''),
+            ],
+        ];
+    }
+}
