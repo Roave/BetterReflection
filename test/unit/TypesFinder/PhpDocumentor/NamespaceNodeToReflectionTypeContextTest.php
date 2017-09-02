bfb5ffac3793ab51929014668f7a30cdf2192ad1
Marco Pivetta <ocramius@gmail.com>
2017-09-02 23:54:00 +0200

    Testing complex scenario with type resolutions, imported constants and functions, and grouped use statements

--- test/unit/TypesFinder/PhpDocumentor/NamespaceNodeToReflectionTypeContextTest.php
+++ test/unit/TypesFinder/PhpDocumentor/NamespaceNodeToReflectionTypeContextTest.php
@@ -4,8 +4,13 @@ declare(strict_types=1);
 namespace Roave\BetterReflectionTest\TypesFinder\PhpDocumentor;
 
 use phpDocumentor\Reflection\Types\Context;
+use PhpParser\Builder\Use_;
 use PhpParser\Node\Name;
+use PhpParser\Node\Stmt\Class_;
+use PhpParser\Node\Stmt\GroupUse;
 use PhpParser\Node\Stmt\Namespace_;
+use PhpParser\Node\Stmt\Use_ as UseStatement;
+use PhpParser\Node\Stmt\UseUse;
 use PHPUnit\Framework\TestCase;
 use Roave\BetterReflection\TypesFinder\PhpDocumentor\NamespaceNodeToReflectionTypeContext;
 
@@ -24,6 +29,31 @@ class NamespaceNodeToReflectionTypeContextTest extends TestCase
 
     public function expectedContextsProvider() : array
     {
+        $namespaceWithImports = new Namespace_(
+            new Name('With\\Imports'),
+            [
+                (new Use_('ClassName', UseStatement::TYPE_NORMAL))->getNode(),
+                (new Use_('ConstantName', UseStatement::TYPE_CONSTANT))->getNode(),
+                (new Use_('FunctionName', UseStatement::TYPE_FUNCTION))->getNode(),
+                (new Use_('UnknownName', UseStatement::TYPE_UNKNOWN))->getNode(),
+                (new Use_('AAA\\BBB', UseStatement::TYPE_NORMAL))->getNode(),
+                (new Use_('BBB\\CCC\\DDD', UseStatement::TYPE_NORMAL))->getNode(),
+                (new Use_('Foo\\EEE\\FFF', UseStatement::TYPE_NORMAL))->getNode(),
+                (new Use_('Foo', UseStatement::TYPE_NORMAL))->getNode(),
+                (new Use_('GGG', UseStatement::TYPE_NORMAL))->as('HHH')->getNode(),
+                (new Use_('III', UseStatement::TYPE_NORMAL))->as('JJJ')->getNode(),
+                new GroupUse(
+                    new Name('LLL'),
+                    [
+                        new UseUse(new Name('MMM')),
+                        new UseUse(new Name('NNN'), 'OOO'),
+                    ]
+                ),
+                (new Use_('\\PPP', UseStatement::TYPE_NORMAL))->getNode(),
+                new Class_('ClassNode'), // class node, should be ignored
+            ]
+        );
+
         return [
             'No namespace' => [
                 null,
@@ -41,6 +71,25 @@ class NamespaceNodeToReflectionTypeContextTest extends TestCase
                 new Namespace_(new Name('\\Foo\\Bar')),
                 new Context('Foo\\Bar'),
             ],
+            'Complex use statement (with fucked up group statements - yes, I do hate those)' => [
+                $namespaceWithImports,
+                new Context(
+                    'With\\Imports',
+                    [
+                        'ClassName' => 'ClassName',
+                        'UnknownName' => 'UnknownName',
+                        'BBB' => 'AAA\\BBB',
+                        'DDD' => 'BBB\\CCC\\DDD',
+                        'FFF' => 'Foo\\EEE\\FFF',
+                        'Foo' => 'Foo',
+                        'HHH' => 'GGG',
+                        'JJJ' => 'III',
+                        'MMM' => 'LLL\\MMM',
+                        'OOO' => 'LLL\\NNN',
+                        'PPP' => 'PPP',
+                    ]
+                ),
+            ]
         ];
     }
 }
