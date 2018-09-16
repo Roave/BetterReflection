66b23d5bc46dcb37f81ff4abb9a600b467bfa122
Tribusean Ilya <slash3b@gmail.com>
2018-09-16 12:08:40 +0300

    path fixes in 'parsing-whole-directory' examples

--- demo/parsing-whole-directory/example1.php
+++ demo/parsing-whole-directory/example1.php
@@ -2,13 +2,13 @@
 
 # parse all classes in a directory
 
-require_once __DIR__ . '/vendor/autoload.php';
+require_once __DIR__ . '/../../vendor/autoload.php';
 
 use Roave\BetterReflection\BetterReflection;
 use Roave\BetterReflection\Reflector\ClassReflector;
 use Roave\BetterReflection\SourceLocator\Type\DirectoriesSourceLocator;
 
-$directories = [__DIR__ . '/src'];
+$directories = [__DIR__ . '/../../src'];
 
 $sourceLocator = new DirectoriesSourceLocator(
     $directories,
