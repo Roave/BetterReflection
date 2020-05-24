eb9bd0e2ebcb2e04e57cbf4e9bc9583079c63ec0
Jaroslav Hanslík <kukulich@kukulich.cz>
2020-05-24 19:28:27 +0200

    Fixed ReflectionMethod::isAbstract() in adapter

--- src/Reflection/Adapter/ReflectionMethod.php
+++ src/Reflection/Adapter/ReflectionMethod.php
@@ -273,7 +273,7 @@ class ReflectionMethod extends CoreReflectionMethod
      */
     public function isAbstract()
     {
-        return $this->betterReflectionMethod->isAbstract();
+        return $this->betterReflectionMethod->isAbstract() || $this->betterReflectionMethod->getDeclaringClass()->isInterface();
     }
 
     /**
