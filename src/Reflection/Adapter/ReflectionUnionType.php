80f0a84857e05684dcfcdeec1e6aaa3824d7afdb
Jaroslav Hanslík <kukulich@kukulich.cz>
2021-10-12 23:56:29 +0200

    Updated locked dependencies

--- src/Reflection/Adapter/ReflectionUnionType.php
+++ src/Reflection/Adapter/ReflectionUnionType.php
@@ -21,7 +21,7 @@ class ReflectionUnionType extends CoreReflectionUnionType
      */
     public function getTypes(): array
     {
-        return array_map(static fn(BetterReflectionNamedType|BetterReflectionUnionType $type): ReflectionNamedType|ReflectionUnionType|null => ReflectionType::fromTypeOrNull($type), $this->betterReflectionType->getTypes());
+        return array_map(static fn (BetterReflectionNamedType|BetterReflectionUnionType $type): ReflectionNamedType|ReflectionUnionType|null => ReflectionType::fromTypeOrNull($type), $this->betterReflectionType->getTypes());
     }
 
     public function __toString(): string
