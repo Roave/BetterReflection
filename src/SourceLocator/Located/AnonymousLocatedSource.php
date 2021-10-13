b848b4f86857f5d7970707f6c4276ae27ee75647
Jaroslav Hanslík <kukulich@kukulich.cz>
2021-10-13 14:58:50 +0200

    Mark all located sources as internal

--- src/SourceLocator/Located/AnonymousLocatedSource.php
+++ src/SourceLocator/Located/AnonymousLocatedSource.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace Roave\BetterReflection\SourceLocator\Located;
 
+/**
+ * @internal
+ */
 class AnonymousLocatedSource extends LocatedSource
 {
     public function __construct(string $source, string $filename)
--- src/SourceLocator/Located/InternalLocatedSource.php
+++ src/SourceLocator/Located/InternalLocatedSource.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace Roave\BetterReflection\SourceLocator\Located;
 
+/**
+ * @internal
+ */
 class InternalLocatedSource extends LocatedSource
 {
     public function __construct(string $source, string $name, private string $extensionName)
