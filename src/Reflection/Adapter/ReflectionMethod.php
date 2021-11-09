c4d75824b8e45c184db04cdb9591bbe002588f6c
Jaroslav Hanslík <kukulich@kukulich.cz>
2021-11-09 21:57:05 +0100

    ReflectionMethod adapter cleanup

--- src/Reflection/Adapter/ReflectionMethod.php
+++ src/Reflection/Adapter/ReflectionMethod.php
@@ -14,6 +14,7 @@ use Roave\BetterReflection\Reflection\Adapter\Exception\NotImplemented;
 use Roave\BetterReflection\Reflection\Exception\NoObjectProvided;
 use Roave\BetterReflection\Reflection\ReflectionAttribute as BetterReflectionAttribute;
 use Roave\BetterReflection\Reflection\ReflectionMethod as BetterReflectionMethod;
+use Roave\BetterReflection\Reflection\ReflectionParameter as BetterReflectionParameter;
 use Roave\BetterReflection\Util\FileHelper;
 use Throwable;
 use TypeError;
@@ -130,14 +131,10 @@ final class ReflectionMethod extends CoreReflectionMethod
      */
     public function getParameters(): array
     {
-        $parameters = $this->betterReflectionMethod->getParameters();
-
-        $wrappedParameters = [];
-        foreach ($parameters as $key => $parameter) {
-            $wrappedParameters[$key] = new ReflectionParameter($parameter);
-        }
-
-        return $wrappedParameters;
+        return array_map(
+            static fn (BetterReflectionParameter $parameter): ReflectionParameter => new ReflectionParameter($parameter),
+            $this->betterReflectionMethod->getParameters(),
+        );
     }
 
     public function hasReturnType(): bool
