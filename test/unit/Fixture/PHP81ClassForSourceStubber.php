ee8e7b2f6fe0ac1ddbfc4f45883b76b809b83b53
Jaroslav Hanslík <kukulich@kukulich.cz>
2021-11-15 08:58:27 +0100

    Added tests of intersection types for ReflectionSourceStubber

--- test/unit/Fixture/PHP81ClassForSourceStubber.php
+++ test/unit/Fixture/PHP81ClassForSourceStubber.php
@@ -2,7 +2,14 @@
 
 namespace Roave\BetterReflectionTest\Fixture;
 
+use ArrayIterator;
+use stdClass;
+
 class PHP81ClassForSourceStubber
 {
     public final const FINAL_CONST = 'finalConst';
+
+    public function getIntersectionType(): ArrayIterator&stdClass
+    {
+    }
 }
--- test/unit/Fixture/PHP81ClassForSourceStubberExpected.php
+++ test/unit/Fixture/PHP81ClassForSourceStubberExpected.php
@@ -5,4 +5,7 @@ namespace Roave\BetterReflectionTest\Fixture;
 class PHP81ClassForSourceStubber
 {
     public final const FINAL_CONST = 'finalConst';
+    public function getIntersectionType() : \ArrayIterator&\stdClass
+    {
+    }
 }
