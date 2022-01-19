cccf77bd48e40d16d91405cbae2908a23516b265
Jaroslav Hanslík <kukulich@kukulich.cz>
2022-01-19 19:34:07 +0100

    Kill mutants in ReflectionType

--- test/unit/Reflection/ReflectionTypeTest.php
+++ test/unit/Reflection/ReflectionTypeTest.php
@@ -35,32 +35,36 @@ class ReflectionTypeTest extends TestCase
     public function dataProvider(): array
     {
         return [
-            [new Node\Name('A'), false, ReflectionNamedType::class, false],
-            [new Node\Identifier('string'), false, ReflectionNamedType::class, false],
+            [new Node\Name('A'), false, ReflectionNamedType::class, 'A', false],
+            [new Node\Identifier('string'), false, ReflectionNamedType::class, 'string', false],
             'Forcing a type to be nullable turns it into a `T|null` ReflectionUnionType' => [
                 new Node\Identifier('string'),
                 true,
                 ReflectionUnionType::class,
+                'string|null',
                 true,
             ],
             'Nullable types are converted into `T|null` ReflectionUnionType instances' => [
                 new Node\NullableType(new Node\Identifier('string')),
                 false,
                 ReflectionUnionType::class,
+                'string|null',
                 true,
             ],
-            [new Node\IntersectionType([new Node\Name('A'), new Node\Name('B')]), false, ReflectionIntersectionType::class, false],
-            [new Node\UnionType([new Node\Name('A'), new Node\Name('B')]), false, ReflectionUnionType::class, false],
+            [new Node\IntersectionType([new Node\Name('A'), new Node\Name('B')]), false, ReflectionIntersectionType::class, 'A&B', false],
+            [new Node\UnionType([new Node\Name('A'), new Node\Name('B')]), false, ReflectionUnionType::class, 'A|B', false],
             'Union types composed of just `null` and a type are kept as `T|null` ReflectionUnionType' => [
                 new Node\UnionType([new Node\Name('A'), new Node\Name('null')]),
                 false,
                 ReflectionUnionType::class,
+                'A|null',
                 true,
             ],
             'Union types composed of `null` and more than one type are kept as `T|U|null` ReflectionUnionType' => [
                 new Node\UnionType([new Node\Name('A'), new Node\Name('B'), new Node\Name('null')]),
                 false,
                 ReflectionUnionType::class,
+                'A|B|null',
                 true,
             ],
         ];
@@ -73,11 +77,13 @@ class ReflectionTypeTest extends TestCase
         Node\Identifier|Node\Name|Node\NullableType|Node\UnionType|Node\IntersectionType $node,
         bool $forceAllowsNull,
         string $expectedReflectionClass,
+        string $expectedTypeAsString,
         bool $expectedAllowsNull,
     ): void {
         $reflectionType = ReflectionType::createFromNode($this->reflector, $this->owner, $node, $forceAllowsNull);
 
         self::assertInstanceOf($expectedReflectionClass, $reflectionType);
+        self::assertSame($expectedTypeAsString, $reflectionType->__toString());
         self::assertSame($expectedAllowsNull, $reflectionType->allowsNull());
         self::assertSame($this->owner, $reflectionType->getOwner());
     }
