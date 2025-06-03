f5c6d447b18f8ee6cb72fa1d3200246efbdef591
Maks Rafalko <maks.rafalko@gmail.com>
2025-06-03 23:13:55 +0200

    Use infection/infection dev-master

--- test/unit/SourceLocator/FileCheckerTest.php
+++ test/unit/SourceLocator/FileCheckerTest.php
@@ -35,7 +35,7 @@ class FileCheckerTest extends TestCase
         FileChecker::assertReadableFile(__DIR__);
     }
 
-    public function CheckFileThrowsExceptionIfFileIsNotReadable(): void
+    public function testCheckFileThrowsExceptionIfFileIsNotReadable(): void
     {
         if (str_starts_with(PHP_OS, 'WIN')) {
             self::markTestSkipped('It\'s not possible to change file mode on Windows');
