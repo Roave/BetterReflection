26ca89c012aad6fe456eb3a874579700a64fd931
Vincent Langlet <vincentlanglet@hotmail.fr>
2026-04-07 13:11:17 +0200

    Add Unit test

---
+++ test/unit/Fixture/ReadonlyExampleClass.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Roave\BetterReflectionTest\Fixture;
+
+readonly class ReadonlyExampleClass
+{
+    private int $privateProperty;
+
+    protected bool $protectedProperty;
+
+    public string $publicProperty;
+}
