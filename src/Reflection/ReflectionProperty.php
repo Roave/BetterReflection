2bbb1dea6e0354ed0e2c71619ab308902a2e2e97
Vincent Langlet <vincentlanglet@hotmail.fr>
2026-04-07 13:11:37 +0200

    Override set compatibility if the class is readonly

--- src/Reflection/ReflectionProperty.php
+++ src/Reflection/ReflectionProperty.php
@@ -688,7 +688,10 @@ class ReflectionProperty
             ! ($modifiers & (ReflectionPropertyAdapter::IS_PROTECTED_SET_COMPATIBILITY | ReflectionPropertyAdapter::IS_PRIVATE_SET_COMPATIBILITY))
             && ! $node->isPublicSet()
             && $node->isPublic()
-            && ($modifiers & ReflectionPropertyAdapter::IS_READONLY)
+            && (
+                ($modifiers & ReflectionPropertyAdapter::IS_READONLY)
+                || $this->getDeclaringClass()->isReadOnly()
+            )
         ) {
             $modifiers += ReflectionPropertyAdapter::IS_PROTECTED_SET_COMPATIBILITY;
         }
