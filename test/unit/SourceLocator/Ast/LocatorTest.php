11947679d6b9f9696b70a669c98a5c43d14b61e9
Abdul Malik Ikhsan <samsonasik@gmail.com>
2026-09-10 09:36:18 +0700

    Use StructArmed on QA

--- test/unit/SourceLocator/Ast/LocatorTest.php
+++ test/unit/SourceLocator/Ast/LocatorTest.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace Roave\BetterReflectionTest\Reflector;
+namespace Roave\BetterReflectionTest\SourceLocator\Ast;
 
 use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\TestCase;
